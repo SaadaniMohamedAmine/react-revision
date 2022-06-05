549b06c20a0d3059140870bc86ffcc9ffdbc7e1b
Saadani Mohamed Amine <mohamedaminesaadani79@gmail.com>
2022-06-05 15:24:39 +0200

    feat:remove unsessary files

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-stone-100 text-center py-2">
+      <h1>This is my application</h1>
+    </div>
+  );
+};
+
+export default Navbar;
